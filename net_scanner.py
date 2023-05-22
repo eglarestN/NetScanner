9f18e1121c0f843ecb7431718c08ed35c429d878
Emir Yıldırım <94414298+eglarestN@users.noreply.github.com>
2023-05-22 21:09:46 +0300

    net_scanner.py

--- net_scanner.py
+++ net_scanner.py
@@ -1,30 +1,30 @@
-# paketleri oluşturmak, göndermek ve yakalamak için scapy kütüphanesini import ettim
+# paketleri olusturmak, gondermek ve yakalamak icin scapy kutuphanesini import ettim
 import scapy.all as scapy
-# kullanıcıdan ip adresi almak için optparse kütüphanesini kullanırım
+# kullanicidan ip adresi almak icin optparse kutuphanesini kullanirim
 import optparse
 
 
 def ip_adresi():
-    # burada parse objemi oluştururum
+    # burada parse objemi olustururum
     parse_object = optparse.OptionParser()
-    # burada kullanıcıdan almak istediğimiz kadar keyword arguments alırım
-    # burada kullanıcı isterse -i veya --ipaddress ile taramak  istediğimiz ip adresini veririz
-    # buradaki dest kısmında alınan girdiyi hangi veriye kaydetmek istediğimi belirtirim
-    parse_object.add_option("-i", "--ipaddress", dest="ip_address", help="Lütfen bir ip adresi giriniz")
+    # burada kullanicidan almak istedigimiz kadar keyword arguments alirim
+    # burada kullanici isterse -i veya --ipaddress ile taramak  istedigimiz ip adresini veririz
+    # buradaki dest kisminda alinan girdiyi hangi veriye kaydetmek istedigimi belirtirim
+    parse_object.add_option("-i", "--ipaddress", dest="ip_address", help="Lutfen bir ip adresi giriniz")
 
     (girilen_ip_adresi, arguments) = parse_object.parse_args()
-    # burada eğer kullanıcı ip adresi girmediyse ona bir hata mesajı gösteririm
+    # burada eger kullanici ip adresi girmediyse ona bir hata mesaji gosteririm
     if not girilen_ip_adresi.ip_address:
-        print("Lütfen bir ip adresi giriniz")
+        print("Lutfen bir ip adresi giriniz")
 
     return girilen_ip_adresi
 
 
-# bu fonksiyonda cevap, istek ve yayın paketlerimi oluştururum
+# bu fonksiyonda cevap, istek ve yayin paketlerimi olustururum
 def ag_taramasi(ip):
-    # burada ARP paketimi oluştururum (ARP= Address Resolution Protocol-> adres çözümleme protokolü)
+    # burada ARP paketimi olustururum (ARP= Address Resolution Protocol-> adres cozumleme protokolu)
     arp_istek_paketi = scapy.ARP(pdst=ip)
-    # modeme bir yayın istedği göndermek için bir yayın paketi oluşturuyoruz
+    # modeme bir yayin istegi gondermek icin bir yayin paketi olusturuyoruz
     yayin_paketi = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     birlesmis_paket = yayin_paketi / arp_istek_paketi
     (cevaplanalar, cevaplananlar) = scapy.srp(birlesmis_paket, timeout=1)
