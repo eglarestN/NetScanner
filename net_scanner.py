56fdf2288554d10d41c34fcff1ea6641dbdb0aad
Emir Yıldırım <94414298+eglarestN@users.noreply.github.com>
2023-05-23 14:33:30 +0300

    net_scanner.py

--- net_scanner.py
+++ net_scanner.py
@@ -1,36 +1,41 @@
-# paketleri olusturmak, gondermek ve yakalamak icin scapy kutuphanesini import ettim
+#paketleri olusturmak, gondermek ve yakalamak icin scapy kutuphanesini import ettim
 import scapy.all as scapy
-# kullanicidan ip adresi almak icin optparse kutuphanesini kullanirim
+#kullanicidan ip adresi ve dosya adi almak icin optparse kutuphanesini kullandim
 import optparse
 
 
 def ip_adresi():
-    # burada parse objemi olustururum
+    #burada bir parse objesi olustururum
     parse_object = optparse.OptionParser()
     # burada kullanicidan almak istedigimiz kadar keyword arguments alirim
     # burada kullanici isterse -i veya --ipaddress ile taramak  istedigimiz ip adresini veririz
     # buradaki dest kisminda alinan girdiyi hangi veriye kaydetmek istedigimi belirtirim
     parse_object.add_option("-i", "--ipaddress", dest="ip_address", help="Lutfen bir ip adresi giriniz")
-
+    parse_object.add_option("-w", "--write", dest="dosya_adi", help="Kaydedilecek dosyanin adini belirtin")
     (girilen_ip_adresi, arguments) = parse_object.parse_args()
-    # burada eger kullanici ip adresi girmediyse ona bir hata mesaji gosteririm
+
     if not girilen_ip_adresi.ip_address:
         print("Lutfen bir ip adresi giriniz")
 
     return girilen_ip_adresi
 
-
-# bu fonksiyonda cevap, istek ve yayin paketlerimi olustururum
-def ag_taramasi(ip):
-    # burada ARP paketimi olustururum (ARP= Address Resolution Protocol-> adres cozumleme protokolu)
+#bu fonksiyonda cevap, istek ve yayin paketlerimi olustururum
+def ag_taramasi(ip, dosya_adi=None):
     arp_istek_paketi = scapy.ARP(pdst=ip)
-    # modeme bir yayin istegi gondermek icin bir yayin paketi olusturuyoruz
+    #modeme bir yayin istegi gondermek icin bir yayin paketi olusturdum
     yayin_paketi = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     birlesmis_paket = yayin_paketi / arp_istek_paketi
-    (cevaplananlar, cevaplanmayanlar) = scapy.srp(birlesmis_paket, timeout=1)
+    cevaplananlar, cevaplanmayanlar = scapy.srp(birlesmis_paket, timeout=1)
+    #buradaki summary metodu bir paketin onemli bilgilerini ozet seklinde bize sunar
     cevaplananlar.summary()
+
+#buradaki if kosulunda eger kullanici dosya adi verip kaydetmek istediyse sonuclari bir txt dosyasina kaydeder
+    if dosya_adi:
+        with open(dosya_adi, 'w') as dosya:
+            for cevap in cevaplananlar:
+                dosya.write(cevap[1].summary() + "\n")
+        print("Sonuclar", dosya_adi, "dosyasina kaydedildi.")
 
 
 kullanici_ip_adresi = ip_adresi()
-ag_taramasi(kullanici_ip_adresi.ip_address)
-
+ag_taramasi(kullanici_ip_adresi.ip_address, kullanici_ip_adresi.dosya_adi)
